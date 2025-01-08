0b415fc5ef114e207fefff5bb16d596fe06e13ce
Rahul <rahulbodh819@gmail.com>
2025-01-08 22:36:32 +0530

    contect item desing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,4 +50,8 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+
+    implementation(libs.circleimageview)
+
 }
